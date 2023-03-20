f6ae8bd372802f3750cf1aebb8452b79baad7e70
des0lation <72014077+des0lation@users.noreply.github.com>
2023-03-21 08:44:14 +1100

    Fixing Slider

--- main.py
+++ main.py
@@ -79,7 +79,9 @@ st.write("Currently Emmissions per $ is", emm_per_dollar)
 total_supply = 58579243
 aura_supply = []
 default_value = 100 * result2 / result
-st.session_state['aura_share'] = st.slider("Select projected AURA veBAL %share", min_value=0.0, max_value=100.0,value = default_value,step=0.1, format="%f") / 100
+with st.form("Select your Predicted Aura Share"):
+    st.session_state['aura_share'] = st.slider("Select projected AURA veBAL %share", min_value=0.0, max_value=100.0,value=default_value,step=0.1, format="%f") / 100
+
 aura_revenue = []
 
 # Revenue Daily Numbers
