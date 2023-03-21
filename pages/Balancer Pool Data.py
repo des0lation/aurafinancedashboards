ed8f6d3455599632f5dd010587981419473a9e49
des0lation <72014077+des0lation@users.noreply.github.com>
2023-03-22 01:06:29 +1100

    First Draft on LST Pools4

--- pages/Balancer Pool Data.py
+++ pages/Balancer Pool Data.py
@@ -75,8 +75,9 @@ def get_all_weights():
     for key in lst_pools.keys():
         time.sleep(1)
         weights.append(int(getgaugeweight(lst_pools[key]))/10**18)
-        weight_values.append(aurabal_price/10**18*result*int(getgaugeweight(lst_pools[key]))/10**18)
-        ve_bals.append(result*int(getgaugeweight(lst_pools[key]))/10**18)
+        gauge_weight = getgaugeweight(lst_pools[key])
+        weight_values.append(aurabal_price/10**18*result*int(gauge_weight)/10**18)
+        ve_bals.append(result*int(gauge_weight)/10**18)
     return weights, weight_values
 
 
