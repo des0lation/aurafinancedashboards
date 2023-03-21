0d9667e954f25a0c005c44f4a2dd39fda23360c0
des0lation <72014077+des0lation@users.noreply.github.com>
2023-03-21 20:45:36 +1100

    First Draft on LST Pools4

--- pages/page1.py
+++ pages/page1.py
@@ -70,6 +70,6 @@ def get_all_weights():
     return weights
 
 weights_list = get_all_weights()
-df = pd.DataFrame({"Pool": lst_pools.keys(), "Address": lst_pools.values(),"veBAl Weights":weights_list}).sort_values(by = 'veBal Weights', ascending=False)
+df = pd.DataFrame({"Pool": lst_pools.keys(), "Address": lst_pools.values(),"veBAl Weights":weights_list}).sort_values(by = 'veBAL Weights', ascending=False)
 st.dataframe(df)
 
