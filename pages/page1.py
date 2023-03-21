7162befe093ffe3799a55a8948977faf8cf1217d
des0lation <72014077+des0lation@users.noreply.github.com>
2023-03-21 20:28:38 +1100

    First Draft on LST Pools4

--- pages/page1.py
+++ pages/page1.py
@@ -67,6 +67,7 @@ def get_all_weights():
         weights.append(int(getgaugeweight(lst_pools[key]))/10**18)
     return weights
 
+st.write(len(weights))
 df = pd.DataFrame({"Pool": lst_pools.keys(), "Address": lst_pools.values(),"veBAl Weights":weights})
 st.dataframe(df)
 
