e0835b954bf9212bec7482d4f08d81bb5f847a94
des0lation <72014077+des0lation@users.noreply.github.com>
2023-03-22 00:59:00 +1100

    First Draft on LST Pools4

--- pages/Balancer Pool Data.py
+++ pages/Balancer Pool Data.py
@@ -109,6 +109,8 @@ for key in bal_pools_keys:
             founds_symbols.append(str(pool['symbol']+'-gauge'))
             pools_liquidity.append(pool['totalLiquidity'])
 
+st.write(lst_pools.keys())
+st.write(len(lst_pools.keys()))
 
 
 df = pd.DataFrame({"Pool": lst_pools.keys(), "Address": lst_pools.values(),"veBAl Weights":weights_list,"veBAL":ve_bals, "veBAL value":weight_values,"Liquidity":pools_liquidity})
