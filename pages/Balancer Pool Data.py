d146b05db8c474b02f25b4935f8b1ab06c13eada
des0lation <72014077+des0lation@users.noreply.github.com>
2023-03-22 10:05:55 +1100

    First Draft on LST Pools4

--- pages/Balancer Pool Data.py
+++ pages/Balancer Pool Data.py
@@ -156,7 +156,7 @@ st.write("From the Pools Collect, each % of veBAL is on median generating",stati
 st.write("So we are rougly generating $1 million of liquidity per % of veBAL we are owning which has a market value of",0.01 * aurabal_price* result/10**18)
 st.write("Looking at just LST metastable pools",extracted_lst_pools,"we get an average of", statistics.mean(justlstsavg))
 
-veBAL_values = list(range(0, 101))
+veBAL_values = list(range(0, 11))
 liquidity = [statistics.mean(justlstsavg) * i for i in veBAL_values]
 fig = px.line( x=veBAL_values, y=liquidity, labels={'veBAL': '% of veBAL', 'liquidity': 'Liquidity'})
 st.plotly_chart(fig)
