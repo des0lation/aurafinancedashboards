3f42654c60c7630853cb39435765bfb9449c81b3
des0lation <72014077+des0lation@users.noreply.github.com>
2023-03-22 09:10:39 +1100

    First Draft on LST Pools4

--- pages/Balancer Pool Data.py
+++ pages/Balancer Pool Data.py
@@ -143,7 +143,7 @@ st.dataframe(df, width=None)
 
 justlstsavg = []
 for i,weight in enumerate(weights_list):
-    str(founds_symbols[i].replace('-gauge', ''))
+    st.write(str(founds_symbols[i].replace('-gauge', '')))
     try:
         if str(founds_symbols[i].replace('-gauge','')) in bribe_pools_sorted:
             justlstsavg.append(pools_liquidity[i]/(100*weight))
