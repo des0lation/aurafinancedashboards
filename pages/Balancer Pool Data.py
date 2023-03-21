38415424481185d185f4ee30650dae511f0eb618
des0lation <72014077+des0lation@users.noreply.github.com>
2023-03-22 10:32:00 +1100

    First Draft on LST Pools4

--- pages/Balancer Pool Data.py
+++ pages/Balancer Pool Data.py
@@ -157,7 +157,7 @@ liquidity = [statistics.mean(justlstsavg) * i for i in veBAL_values]
 fig = px.line(x=veBAL_values, y=liquidity, labels={'x': '% of veBAL', 'y': 'Liquidity'})
 st.plotly_chart(fig)
 
-
+total_vl_aura = getvlaura()
 investment = st.slider('Select a value', min_value=0, max_value=10000000, step=1000)
 vl_aura_amount = investment/aura_price
 voting_power = vl_aura_amount/(total_vl_aura+vl_aura_amount)*result2 / 10 ** 18
