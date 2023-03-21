46da8c272aae919fa3776d1f580b7021e7304705
des0lation <72014077+des0lation@users.noreply.github.com>
2023-03-22 09:11:53 +1100

    First Draft on LST Pools4

--- pages/Balancer Pool Data.py
+++ pages/Balancer Pool Data.py
@@ -144,9 +144,9 @@ st.dataframe(df, width=None)
 justlstsavg = []
 st.write(bribe_pools_sorted)
 for i,weight in enumerate(weights_list):
-    st.write(str(founds_symbols[i].replace('-gauge', '')))
+    st.write("MetaStable " + str(founds_symbols[i].replace('-gauge', '')))
     try:
-        if str(founds_symbols[i].replace('-gauge','')) in bribe_pools_sorted:
+        if str("MetaStable " + founds_symbols[i].replace('-gauge','')) in bribe_pools_sorted:
             justlstsavg.append(pools_liquidity[i]/(100*weight))
     except:
         continue
