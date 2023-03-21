883988ffe4dafdeb4747f3a7aace7c63a33dafca
des0lation <72014077+des0lation@users.noreply.github.com>
2023-03-22 00:43:33 +1100

    First Draft on LST Pools4

--- pages/Balancer Pool Data.py
+++ pages/Balancer Pool Data.py
@@ -105,6 +105,8 @@ for key in bal_pools_keys:
         if str(pool['symbol']+'-gauge') in lst_pools.keys():
             pools_liquidity.append(pool['totalLiquidity'])
 
+print(len(pools_liquidity))
+
 df = pd.DataFrame({"Pool": lst_pools.keys(), "Address": lst_pools.values(),"veBAl Weights":weights_list,"veBAL":ve_bals, "veBAL value":weight_values,"Liquidity":pools_liquidity})
 df = df.sort_values(by ="veBAl Weights", ascending=False)
 df['Liquidity per veBAL'] = df['veBAL']/df['veBAL Weights']
