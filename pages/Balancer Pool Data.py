94c367dc859343c5d91f2db9ae2fda987811561d
des0lation <72014077+des0lation@users.noreply.github.com>
2023-03-22 11:33:18 +1100

    First Draft on LST Pools4

--- pages/Balancer Pool Data.py
+++ pages/Balancer Pool Data.py
@@ -195,6 +195,7 @@ tvl = st.slider('Select a TVL', min_value=1, max_value=100000000,value = 1000000
 bribes = st.slider("Bribing?",min_value = 0 , max_value = 20000,step = 1000,format='%.2f')
 st.write("This would lead to a bribing expense of",bribes*52,"$ per year")
 auralockpercentage = st.slider("How much of AURA minted each week is being deposited into vlAURA?",min_value = 0,max_value = 100,format='%.2f')
+new_aura_investments = st.slider("How much AURA will be buy each week to combat vlAURA dilution",min_value = 0,max_value = 1000,format='%.2f')
 vl_aura_amount = (investment + bribes*votes_per_dollar)/aura_price
 voting_power = vl_aura_amount/(total_vl_aura+vl_aura_amount)*result2 / 10 ** 18
 vebal_percentage = 100*voting_power/(result / 10 ** 18)
@@ -210,6 +211,7 @@ running_total = 0
 voting_power_loss = []
 for i,auraearned in enumerate(aura_revenue):
     total_vl_aura = total_vl_aura + auraearned * auralockpercentage/100
+    vl_aura_amount = vl_aura_amount + new_aura_investments
     voting_power = vl_aura_amount/(total_vl_aura+vl_aura_amount)*result2 / 10 ** 18
     vebal_percentage = 100 * voting_power / (result / 10 ** 18)
     running_total += auraearned * vebal_percentage/100
