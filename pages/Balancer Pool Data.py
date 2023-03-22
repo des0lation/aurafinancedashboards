e96c59272e9407834afcf520caa677087810c8ca
des0lation <72014077+des0lation@users.noreply.github.com>
2023-03-22 16:37:53 +1100

    First Draft on LST Pools4

--- pages/Balancer Pool Data.py
+++ pages/Balancer Pool Data.py
@@ -215,13 +215,13 @@ aprs = []
 running_total = 0
 voting_power_loss = []
 for i,auraearned in enumerate(aura_revenue):
-    dilution = (auraearned * auralockpercentage/100)/total_vl_aura
-    st.write(dilution)
     total_vl_aura = total_vl_aura + auraearned * auralockpercentage/100
     vl_aura_amount = vl_aura_amount + new_aura_investments
     voting_power = vl_aura_amount/(total_vl_aura)* result2 / 10 ** 18
     vebal_percentage = 100 * voting_power / (result / 10 ** 18)
     running_total += auraearned * vebal_percentage/100
+    dilution = (auraearned * auralockpercentage/100)/total_vl_aura
+    st.write(dilution)
     aprs.append(100*52*((aura_price*auraearned+float(dfmain['Bal Released'][i]) * bal_price) * vebal_percentage/100)/supported_liquidity)
     liq_aura_earned.append(running_total)
 
