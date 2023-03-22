43c9e1770063b8b5cd49ee72fb6607c8294defc5
des0lation <72014077+des0lation@users.noreply.github.com>
2023-03-22 11:53:18 +1100

    First Draft on LST Pools4

--- pages/Balancer Pool Data.py
+++ pages/Balancer Pool Data.py
@@ -224,14 +224,14 @@ for i,auraearned in enumerate(aura_revenue):
     liq_aura_earned.append(running_total)
 
 col1, col2 = st.columns(2)
-years = years = [pd.to_datetime(x, format="%Y/%m/%d").year for x in dfmain['Weeks']]
+weeks = dfmain['Weeks']
 
 with col1:
-    fig2 = px.line(x=years, y=liq_aura_earned, labels={'x': 'Weeks', 'y': 'Aura Earned by Liquidity Pool'})
+    fig2 = px.line(x=weeks, y=liq_aura_earned, labels={'x': 'Weeks', 'y': 'Aura Earned by Liquidity Pool'})
     fig2.update_layout(xaxis_tickangle = 60)
     st.plotly_chart(fig2)
 with col2:
-    fig3 = px.line(x=years, y=aprs, labels={'x': 'Weeks', 'y': 'APR'})
+    fig3 = px.line(x=weeks, y=aprs, labels={'x': 'Weeks', 'y': 'APR'})
     fig3.update_layout(xaxis_tickangle = 60)
     st.plotly_chart(fig3)
 
