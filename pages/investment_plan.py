cca668bc2687e8bfa65d612192df83a0d933dacf
des0lation <72014077+des0lation@users.noreply.github.com>
2023-04-01 15:01:56 +1100

    Adding stETH/WETH liquidity growth2

--- pages/investment_plan.py
+++ pages/investment_plan.py
@@ -35,9 +35,15 @@ pool_data = get_pool_history(pool,timestamp)
 
 liquidity_list = []
 timestamp_list = []
+volume_list = []
 for pool in pool_data:
     liquidity_list.append(pool['liquidity'])
     timestamp_list.append(datetime.datetime.fromtimestamp(pool['timestamp']))
+    volume_list.append(pool['swapVolume'])
+
+fig1 = px.line(x=timestamp_list, y=volume_list, labels={'x': 'Date', 'y': 'stETH/WETH liquidity'})
+fig1.update_layout(xaxis_tickangle=60)
+st.plotly_chart(fig1)
 fig2 = px.line(x=timestamp_list, y=liquidity_list, labels={'x': 'Date', 'y': 'stETH/WETH liquidity'})
 fig2.update_layout(xaxis_tickangle = 60)
 st.plotly_chart(fig2)
